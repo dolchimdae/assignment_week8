92d153584ab895eeda08421e01dd3f28d901278e
dolchimdae <flyingchae@g.skku.edu>
2023-04-20 20:53:36 +0900

    arrange.sh

---
+++ arrange.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+lower=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
+upper=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z")
+
+cd files
+
+for(( i=0; i<26; i++))
+do 
+	mv ${lower[i]}*.txt ../${lower[i]}
+	mv ${upper[i]}*.txt ../${lower[i]}
+done
